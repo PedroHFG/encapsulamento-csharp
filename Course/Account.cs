5c573a400cd494661cc54b60bd2ffaef86c06395
PedroHFG <pedrohfidg@gmail.com>
2024-12-26 14:00:53 -0300

    Exercicio resolvido

---
+++ Course/Account.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Owner { get; set; }
+        public double Balance { get; private set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string owner)
+        {
+            Number = number;
+            Owner = owner;
+        }
+
+        public Account(int number, string owner, double balance) : this(number, owner)
+        {
+            Balance = balance;
+        }
+
+        public void Withdrawal(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public override string? ToString()
+        {
+            return "Conta " + Number
+                + ", Titular: " + Owner
+                + ", Saldo: $ " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account;
+
+            Console.Write("Entre com o número da conta: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string owner = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char choice = char.Parse(Console.ReadLine().ToLower());
+
+            while (choice != 's' && choice != 'n') 
+            {
+                Console.Write("Opção inválida! Escolha uma opção válida (s/n): ");
+                choice = char.Parse(Console.ReadLine().ToLower());
+            }
+
+            if (choice == 's')
+            {
+                Console.Write("Entre com o número do depósito inicial: $ ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(number, owner, initialDeposit);
+            }
+            else
+            {
+                account = new Account(number, owner);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(account);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para depósito: $ ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(amount);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(account);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: $ ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdrawal(amount);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(account);
+        }
+    }
+}
